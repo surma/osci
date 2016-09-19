dfdbc2be05b13007a19a312152cce22c0c775616
Surma <surma@surma.link>
2016-09-19 20:11:20 +0100

    docs(Emulator): Properly define osci CPU

--- osciemu/include/osciemu/emulator.h
+++ osciemu/include/osciemu/emulator.h
@@ -8,20 +8,85 @@ namespace osciemu {
   /**
    * `Emulator` is a class wiring up memory and instruction interpreter
    * to behave like an osci CPU.
+   *
+   * At the start, the bios memory is mapped to the address space
+   * at 2^31 and the instruction pointer (IP) is set to 2^31. By setting
+   * the biosDone (bD) flag to 1, the bios will be unmapped.
+   *
+   * The end of the virtual memory is an area of mapped memory for
+   * control flags, peripherals and interrupts.
+   *
+   * ```
+   * +---------------------------------------+ Address 0
+   * |                 Word 0                |
+   * |                 Word 1                |
+   * |                   ...                 |
+   * +---------------------------------------+ kFlagBoundary
+   * |              Flags Word 0             |
+   * |                   ...                 |
+   * |              Flags Word i             |
+   * +---------------------------------------+ kIvtBoundary
+   * |               IVT Entry 0             |
+   * |                   ...                 |
+   * |               IVT Entry j             |
+   * +---------------------------------------+ kRegisterBoundary
+   * |                Register 0             |
+   * |                   ...                 |
+   * |                Register k             |
+   * +---------------------------------------+ kMaxAddress
+   * ```
+   *
+   * A word is 4 byte. osci CPU always has 2^32 bytes of
+   * virtual memory. Not all the memory is necessarily backed
+   * by physical memory. Reads from purely virtual memory yield
+   * 0. Writes to purely virtual memory are ignored.
+   *
+   * Flags Word 0:
+   *
+   * ```
+   * +-----------------------------------+---+
+   * |    |    |    |    |    |    |    | bD | Byte 0
+   * +---------------------------------------+
+   * |                 Unused                |
+   * +---------------------------------------+
+   * |                 Unused                |
+   * +---------------------------------------+
+   * |                 Unused                | Byte 3
+   * +---------------------------------------+
+   * ```
    */
   class Emulator : public MemoryInterface {
     public:
       /**
        * Constructor.
+       * @param main Main memory of the CPU
+       * @param bios Bios memory
        */
       Emulator(MemoryInterface& main, MemoryInterface& bios);
       ~Emulator();
+      /**
+       * `Step` executes the next instruction of the osci CPU.
+       */
       void Step();
 
+      /**
+       *
+       * @see MemoryInterface::GetSize
+       */
       uint32_t GetSize() const;
+      /**
+       * @see MemoryInterface::GetCell
+       */
       uint8_t GetCell(uint32_t addr) const;
+      /**
+       * @see MemoryInterface::SetCell
+       */
       void SetCell(uint32_t addr, uint8_t value);
 
+      /**
+       * `SetBiosDoneFlag` sets the bD flag to `state`.
+       * @param state State to set the bD flag to
+       */
       void SetBiosDoneFlag(bool state);
 
       static const uint32_t kBiosBoundary = 1<<31;
@@ -31,10 +96,7 @@ namespace osciemu {
       static const uint8_t kNumFlags = 1;
       static const uint32_t kRegisterBoundary = kMaxAddress - kNumRegisters*Instruction::Size;
       static const uint32_t kIvtBoundary = kMaxAddress - (kNumRegisters + kNumIvts)*Instruction::Size;
-      static const uint32_t kFlagBoundary = kIvtBoundary - (kNumFlags + 8 - 1)/8; // = kIvtBoundary - ceil(kNumFlags / 8);
-      enum FlagEntry {
-        kFlagBiosDone
-      };
+      static const uint32_t kFlagBoundary = kIvtBoundary - (kNumFlags + 32 - 1)/32; // = kIvtBoundary - ceil(kNumFlags / 32);
       uint32_t ip_;
 
 
