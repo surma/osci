87b363dfa800f564f5cd5fe081a70348c02b149a
Surma <surma@surma.link>
2016-11-08 21:52:42 +0000

    docs(Emulator): Add `ivt_return` to emulator documentation

--- osciemu/include/osciemu/emulator.h
+++ osciemu/include/osciemu/emulator.h
@@ -30,6 +30,8 @@ namespace osciemu {
    * |               IVT Entry 0             |
    * |                   ...                 |
    * |               IVT Entry j             |
+   * +---------------------------------------+
+   * |           IVT Return address          |
    * +---------------------------------------+ kRegisterBoundary
    * |                Register 0             |
    * |                   ...                 |
