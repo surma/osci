d8a9f688a03e8652cb4fc05cafe9d74359048e52
Surma <surma@surma.link>
2017-07-09 15:15:41 +0100

    feat(Instruction): Implement execute()

--- osci/instruction.nim
+++ osci/instruction.nim
@@ -1,4 +1,4 @@
-from memory import Memory, writeUint32, readUint32
+from memory import Memory, writeUint32, readUint32, readInt32, writeInt32
 
 ##[
 =================
@@ -19,6 +19,8 @@ multiple of the word size.
 osci is a 32-bit little endian CPU and instructions must be serialized accordingly.
 ]##
 
+const INSTRUCTION_SIZE* = 4 * 4
+
 type
   InstructionObj* = object of RootObj
     op_a: uint32
@@ -26,6 +28,9 @@ type
     target: uint32
     jmp: uint32
   Instruction* = ref InstructionObj
+
+proc newInstruction*(op_a, op_b, target, jmp: uint32 = 0): Instruction =
+  Instruction(op_a: op_a, op_b: op_b, target: target, jmp: jmp)
 
 proc serialize*(instr: Instruction, m: Memory, address: uint32) =
   m.writeUint32(address + 00, instr.op_a)
@@ -38,3 +43,14 @@ proc deserialize*(instr: Instruction, m: Memory, address: uint32) =
   instr.op_b = m.readUint32(address + 04)
   instr.target = m.readUint32(address + 08)
   instr.jmp = m.readUint32(address + 12)
+
+proc execute*(instr: Instruction, m: Memory, ip: var uint32) =
+  let
+    op_a = m.readInt32(instr.op_a)
+    op_b = m.readInt32(instr.op_b)
+  let result = op_a - op_b
+  m.writeInt32(instr.target, result)
+  if result < 0:
+    ip = instr.jmp
+  else:
+    ip += INSTRUCTION_SIZE
--- tests/instruction_test.nim
+++ tests/instruction_test.nim
@@ -3,9 +3,9 @@ from ../osci/memory import newArrayMemory
 import unittest
 
 suite "instruction":
-  test "idempotence":
+  test "serialize/deserialize":
     var
-      a, b: Instruction = Instruction()
+      a, b: Instruction = newInstruction()
       am = newArrayMemory(16)
     a.op_a = 0x00010203
     a.op_b = 0x04050607
@@ -17,3 +17,56 @@ suite "instruction":
     check(a.op_b == b.op_b)
     check(a.target == b.target)
     check(a.jmp == b.jmp)
+
+  test "execute - no jmp":
+    var
+      am = newArrayMemory(@[
+        5'u8, 0'u8, 0'u8, 0'u8,
+        4'u8, 0'u8, 0'u8, 0'u8,
+        0'u8, 0'u8, 0'u8, 0'u8,
+      ])
+      ip: uint32 = 0
+      instr: Instruction = newInstruction(0, 4, 8, 0x100)
+    instr.execute(am, ip)
+    check(am.readInt32(0) == 5)
+    check(am.readInt32(4) == 4)
+    check(am.readInt32(8) == 1)
+    check(ip == 16)
+
+  test "execute - jmp":
+    var
+      am = newArrayMemory(@[
+        4'u8, 0'u8, 0'u8, 0'u8,
+        5'u8, 0'u8, 0'u8, 0'u8,
+        0'u8, 0'u8, 0'u8, 0'u8,
+      ])
+      ip: uint32 = 0
+      instr: Instruction = newInstruction(0, 4, 8, 0x100)
+    instr.execute(am, ip)
+    check(am.readInt32(0) == 4)
+    check(am.readInt32(4) == 5)
+    check(am.readInt32(8) == -1)
+    check(ip == 0x100)
+
+  test "execute - negative numbers no jmp":
+    var
+      am = newArrayMemory(12)
+      ip: uint32 = 0
+      instr: Instruction = newInstruction(0, 4, 8, 0x100)
+    am.writeInt32(0, -4)
+    am.writeInt32(4, -5)
+    instr.execute(am, ip)
+    check(am.readInt32(8) == 1)
+    check(ip == 16)
+
+
+  test "execute - negative numbers jmp":
+    var
+      am = newArrayMemory(12)
+      ip: uint32 = 0
+      instr: Instruction = newInstruction(0, 4, 8, 0x100)
+    am.writeInt32(0, -5)
+    am.writeInt32(4, -4)
+    instr.execute(am, ip)
+    check(am.readInt32(8) == -1)
+    check(ip == 0x100)
