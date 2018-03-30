c8ce8395adda959a68817bb55cd675ad09c89d82
Surma <surma@surma.link>
2018-03-31 00:08:48 +0100

    feat(hexloader): Implement hexloader

---
+++ src/loader/hexloader.rs
@@ -0,0 +1,119 @@
+
+use memory::SliceMemory;
+use std::io::{Read, ErrorKind, Error, BufReader, BufRead};
+
+pub fn load<U: Read>(f: &mut U) -> Result<SliceMemory, Error> {
+    let mut vec = Vec::<i32>::new();
+    let buf = BufReader::new(f);
+    for line in buf.lines().map(|l| l.unwrap()) {
+        if line.starts_with("#") {
+            continue;
+        }
+        for chunk in line.split_whitespace() {
+            if chunk.len() == 0 {
+                continue;
+            }
+            if chunk.starts_with("#") {
+                break;
+            }
+            if !chunk.chars().all(|c| c.is_digit(16) || c == '-') {
+                return Err(Error::new(ErrorKind::Other, format!("Word contains non-hexnumeric characters: {}", chunk)));
+            }
+            match i32::from_str_radix(chunk, 16) {
+                Ok(n) => vec.push(n),
+                Err(error) => return Err(Error::new(ErrorKind::Other, error))
+            }
+        }
+    }
+    Ok(SliceMemory::from_slice(vec.into_boxed_slice()))
+}
+
+#[cfg(test)]
+mod tests {
+    use memory::Memory;
+    use std::io::Cursor;
+
+    #[test]
+    fn happy_path() {
+        let mut code = Cursor::new("
+        00010203 70717273 20 103
+        ");
+        let mem = super::load(&mut code).unwrap();
+        assert_eq!(mem.get(0), 0x00010203);
+        assert_eq!(mem.get(1), 0x70717273);
+        assert_eq!(mem.get(2), 0x20);
+        assert_eq!(mem.get(3), 0x103);
+    }
+
+    #[test]
+    fn hex() {
+        let mut code = Cursor::new("
+        A a Bb CCc dDd
+        ");
+        let mem = super::load(&mut code).unwrap();
+        assert_eq!(mem.get(0), 0xA);
+        assert_eq!(mem.get(1), 0xA);
+        assert_eq!(mem.get(2), 0xBB);
+        assert_eq!(mem.get(3), 0xCCC);
+        assert_eq!(mem.get(4), 0xDDD);
+    }
+
+    #[test]
+    fn multiline() {
+        let mut code = Cursor::new("
+        00010203
+        70717273
+        20
+        103
+        ");
+        let mem = super::load(&mut code).unwrap();
+        assert_eq!(mem.get(0), 0x00010203);
+        assert_eq!(mem.get(1), 0x70717273);
+        assert_eq!(mem.get(2), 0x20);
+        assert_eq!(mem.get(3), 0x103);
+    }
+
+    #[test]
+    fn negative_numbers() {
+        let mut code = Cursor::new("
+        -1
+        -100
+        ");
+        let mem = super::load(&mut code).unwrap();
+        assert_eq!(mem.get(0), -0x1);
+        assert_eq!(mem.get(1), -0x100);
+    }
+
+    #[test]
+    fn comments() {
+        let mut code = Cursor::new("
+        #lol 00010203
+        00010203 #lol
+        #70717273
+        20
+        #oki
+        #103
+        ");
+        let mem = super::load(&mut code).unwrap();
+        assert_eq!(mem.get(0), 0x00010203);
+        assert_eq!(mem.get(1), 0x20);
+    }
+
+    #[test]
+    fn invalid_digits() {
+        let mut code = Cursor::new("
+        0001020g
+        ");
+        let mem = super::load(&mut code);
+        assert!(mem.is_err());
+    }
+
+    #[test]
+    fn way_too_many_digits() {
+        let mut code = Cursor::new("
+        0001020123123
+        ");
+        let mem = super::load(&mut code);
+        assert!(mem.is_err());
+    }
+}
--- src/loader/mod.rs
+++ src/loader/mod.rs
@@ -3,5 +3,6 @@ use memory::Memory;
 use std::io::{Read, Error};
 
 pub mod rawloader;
+pub mod hexloader;
 
 pub type Loader<T: Memory> = fn (f: Read) -> Result<T, Error>;
