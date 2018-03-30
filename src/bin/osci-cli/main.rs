52004f29fb4904c3dd1541deb1c8d74530efa3af
Surma <surma@surma.link>
2018-03-30 17:45:11 +0100

    feat(osci-cli): Add allow for infinite execution

--- src/bin/osci-cli/main.rs
+++ src/bin/osci-cli/main.rs
@@ -30,14 +30,16 @@ fn main() {
             (@arg IMAGE: -i --image +required +takes_value "Image to load into memory")
             (@arg BIOS: -b --bios +required +takes_value "BIOS to load")
             (@arg STEP: --step "Walk through in stepping mode")
-            (@arg MAX_STEP: --maxstep +takes_value "Maximum number of CPU cycles")
+            (@arg MAX_STEP: --maxstep +takes_value "Maximum number of CPU cycles (0 means infinite)")
         )
         .get_matches();
 
     let max_steps = matches.value_of("MAX_STEP")
-                            .unwrap_or("1000000");
-    let max_steps = max_steps.parse::<usize>()
-                            .expect("--max-step needs to be a number");
+                        .map(|s| {
+                            s.parse::<usize>()
+                                .expect("--max-step needs to be a number")
+                        })
+                        .unwrap_or(0);
 
     let image_buf = read_file_into_slice(matches.value_of("IMAGE").unwrap())
                             .expect("Could not load image");
@@ -56,7 +58,7 @@ fn main() {
     println!("REGISTERS_START_ADDRESS = 0x{:08X}", osciemu::memory::address::REGISTERS_START_ADDRESS);
     println!("STACK_POINTER_ADDRESS = 0x{:08X}", osciemu::memory::address::STACK_POINTER_ADDRESS);
 
-    for count in 1..max_steps+1 {
+    for count in RangeIterator::new(0, max_steps) {
         if emulator.is_halted() {
             break;
         }
@@ -76,5 +78,33 @@ fn main() {
     }
     if !emulator.is_halted() {
         std::process::exit(1);
+    }
+}
+
+struct RangeIterator {
+    count: usize,
+    max: usize
+}
+
+impl RangeIterator {
+    fn new(start: usize, max: usize) -> RangeIterator {
+        RangeIterator {
+            count: start,
+            max,
+        }
+    }
+}
+
+impl Iterator for RangeIterator {
+    type Item = usize;
+    fn next(&mut self) -> Option<usize> {
+        if self.count == self.max && self.max != 0 {
+            return None;
+        }
+        let result = Some(self.count);
+        if self.count != std::usize::MAX {
+            self.count = self.count + 1;
+        }
+        result
     }
 }
