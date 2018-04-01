960f08ec542e9dab0516910a3eebd509069f8c84
Surma <surma@surma.link>
2018-04-01 16:40:46 +0100

    docs(utils): Update documentation

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,16 @@
+//! Various utils to work with osci.
 use std::io;
 use std::fs::File;
 use std::path::Path;
 use memory::Memory;
 use loader::{hexloader, rawloader, LoadError, Result};
 
+/// Turn a file into a memory.
+///
+/// `load_file` recognizes multiple file formats. A file type is recognized by its file extension and mapped to one of the implemented loaders.
+///
+/// - “.raw”, “.bin”, “.img” or no extension: `rawloader`
+/// - “.hex”: `hexloader`
 pub fn load_file(filename: &Path) -> Result<Box<Memory>> {
     let mut file = File::open(filename)?;
     match Path::new(filename).extension().and_then(|ext| ext.to_str()) {
@@ -16,4 +23,7 @@ pub fn load_file(filename: &Path) -> Result<Box<Memory>> {
     }
 }
 
+/// List of formats supported by `load_file`.
+///
+/// The list contains file extensions that are recognized by `load_file`.
 pub static SUPPORTED_FORMATS: [&str; 4] = ["img", "bin", "raw", "hex"];
