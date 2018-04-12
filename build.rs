b2cb94bf81aa0b193545611409d50546ace6d57f
Surma <surma@surma.link>
2018-04-12 21:44:52 +0200

    chore(Cargo): Build osciwasm TypeScript component when building wasm module

---
+++ build.rs
@@ -0,0 +1,21 @@
+use std::process::Command;
+use std::env;
+use std::path::Path;
+
+fn main() {
+    if env::var("CARGO_CFG_TARGET_ARCH").unwrap_or(String::from("")) != "wasm32" {
+        return;
+    }
+
+    let osciwasm_path = &Path::new("./tools/osciwasm");
+    Command::new("npm")
+        .args(&["i"])
+        .current_dir(osciwasm_path)
+        .status()
+        .unwrap();
+    Command::new("npm")
+        .args(&["run", "build"])
+        .current_dir(osciwasm_path)
+        .status()
+        .unwrap();
+}
